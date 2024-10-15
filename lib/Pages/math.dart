75b9d5b7c551c7cc961507dd30bedaf1f1fc7fd3
LAZINESSITSELF <noname72928@gmail.com>
2024-10-16 00:02:26 +0700

    Minor fix

--- lib/Pages/math.dart
+++ lib/Pages/math.dart
@@ -24,26 +24,6 @@ class MathPage extends StatelessWidget {
               height: 60,
               child: ElevatedButton(
                 onPressed: () {
-                  Navigator.pushNamed(context, '/ourteam');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                ),
-                child: const Text('Our Team', textAlign: TextAlign.center),
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 200,
-              height: 60,
-              child: ElevatedButton(
-                onPressed: () {
                   Navigator.pushNamed(context, '/kalkulator');
                 },
                 style: ElevatedButton.styleFrom(
