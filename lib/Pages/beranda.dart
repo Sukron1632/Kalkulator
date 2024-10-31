3b524752f5015da3c43620a88e4c21efb2830ce6
LAZINESSITSELF <noname72928@gmail.com>
2024-10-31 20:36:00 +0700

    back after logged in

--- lib/Pages/beranda.dart
+++ lib/Pages/beranda.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class BerandaPage extends StatelessWidget {
   @override
@@ -7,76 +8,82 @@ class BerandaPage extends StatelessWidget {
     String? userEmail = FirebaseAuth.instance.currentUser?.email;
     String username = userEmail != null ? userEmail.split('@')[0] : 'unknown user';
 
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Beranda'),
-        shadowColor: Colors.black,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Selamat datang di AppaAja,',
-              style: TextStyle(fontSize: 24),
-            ),
-            // const SizedBox(height: 1),
-            Text(
-              username ,
-              style: const TextStyle(fontSize: 24, color: Color.fromARGB(255, 19, 80, 21)),
-            ),
-            const SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    IconButton(
-                      iconSize: 60,
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/ourteam');
-                      },
-                      icon: const Icon(Icons.group),
-                      color: Colors.black,
-                    ),
-                    const SizedBox(height: 8),
-                    const Text('Our Team', style: TextStyle(fontSize: 16)),
-                  ],
-                ),
-                const SizedBox(width: 60),
-                Column(
-                  children: [
-                    IconButton(
-                      iconSize: 60,
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/stopwatch');
-                      },
-                      icon: const Icon(Icons.timer),
-                      color: Colors.black,
-                    ),
-                    const SizedBox(height: 8),
-                    const Text('Stopwatch', style: TextStyle(fontSize: 16)),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Column(
-              children: [
-                IconButton(
-                  iconSize: 60,
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/website');
-                  },
-                  icon: const Icon(Icons.app_shortcut_rounded),
-                  color: Colors.black,
-                ),
-                const SizedBox(height: 8),
-                const Text('Recommended Sites', style: TextStyle(fontSize: 16)),
-              ],
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: () async {
+        // Keluarkan aplikasi saat tombol kembali ditekan
+        SystemNavigator.pop();
+        return false; // Mengindikasikan bahwa kita tidak ingin kembali ke halaman sebelumnya
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Beranda'),
+          shadowColor: Colors.black,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Selamat datang di AppaAja,',
+                style: TextStyle(fontSize: 24),
+              ),
+              Text(
+                username,
+                style: const TextStyle(fontSize: 24, color: Color.fromARGB(255, 19, 80, 21)),
+              ),
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        iconSize: 60,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/ourteam');
+                        },
+                        icon: const Icon(Icons.group),
+                        color: Colors.black,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text('Our Team', style: TextStyle(fontSize: 16)),
+                    ],
+                  ),
+                  const SizedBox(width: 60),
+                  Column(
+                    children: [
+                      IconButton(
+                        iconSize: 60,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/stopwatch');
+                        },
+                        icon: const Icon(Icons.timer),
+                        color: Colors.black,
+                      ),
+                      const SizedBox(height: 8),
+                      const Text('Stopwatch', style: TextStyle(fontSize: 16)),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 30),
+              Column(
+                children: [
+                  IconButton(
+                    iconSize: 60,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/website');
+                    },
+                    icon: const Icon(Icons.app_shortcut_rounded),
+                    color: Colors.black,
+                  ),
+                  const SizedBox(height: 8),
+                  const Text('Recommended Sites', style: TextStyle(fontSize: 16)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
